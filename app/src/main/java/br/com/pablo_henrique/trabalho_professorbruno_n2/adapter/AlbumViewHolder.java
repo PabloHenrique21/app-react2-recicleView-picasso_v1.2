f4dfab58bc34ae992b8b9ee8b2968df6f388d046
pablo <devpablohenrique@gmail.com>
2018-12-10 09:53:54 -0200

    teste1

--- app/src/main/java/br/com/pablo_henrique/trabalho_professorbruno_n2/adapter/AlbumViewHolder.java
+++ app/src/main/java/br/com/pablo_henrique/trabalho_professorbruno_n2/adapter/AlbumViewHolder.java
@@ -1,4 +1,4 @@
-package br.com.pablo_henrique.trabalho_professorbruno_n2.Adapter;
+package br.com.pablo_henrique.trabalho_professorbruno_n2.adapter;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -9,12 +9,8 @@ import br.com.pablo_henrique.trabalho_professorbruno_n2.R;
 public class AlbumViewHolder extends RecyclerView.ViewHolder {
     final TextView id;
 
-
     public AlbumViewHolder(View view){
         super(view);
         id = view.findViewById(R.id.tvId);
-
     }
-
-
 }
